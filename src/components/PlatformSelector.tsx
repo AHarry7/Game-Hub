5f5462de837667fe0bb37bc0d0b87d329fac7fd5
AHarry7 <adeelharry@gmail.com>
2025-02-09 12:48:03 +0500

    platform filter added

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,8 +1,13 @@
 import usePlatforms from "../hooks/usePlatforms";
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
+import { Platform } from "../hooks/useGames";
 
-const PlatformSelector = () => {
+const PlatformSelector = ({
+  onSelectPlatform,
+}: {
+  onSelectPlatform: (platform: Platform) => void;
+}) => {
   const { data, error } = usePlatforms();
 
   if (error) return null;
@@ -14,7 +19,12 @@ const PlatformSelector = () => {
       </MenuButton>
       <MenuList>
         {data.map((platform) => (
-          <MenuItem key={platform.id}>{platform.name}</MenuItem>
+          <MenuItem
+            key={platform.id}
+            onClick={() => onSelectPlatform(platform)}
+          >
+            {platform.name}
+          </MenuItem>
         ))}
       </MenuList>
     </Menu>
